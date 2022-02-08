3b8d75ebe6bbd206924733deb2963713b81d9ef7
Michael Wagstaff <michaelwagstaff2000@gmail.com>
2022-02-08 16:00:13 +0000

    Added compare

---
+++ ProcessorSim/ProcessorSim/Instructions/Compare.cs
@@ -0,0 +1,19 @@
+using ProcessorSim.HardwareResources;
+namespace ProcessorSim.Instructions;
+
+public class Compare
+{
+    private Register flag, reg1, reg2;
+    public Compare(Register flag, Register register1, Register register2)
+    {
+        this.flag = flag;
+        this.reg1 = register1;
+        this.reg2 = register2;
+    }
+
+    public bool execute()
+    {
+        this.flag.setValue((this.reg1.getValue() == this.reg2.getValue()) ? 1 : 0);
+        return true;
+    }
+}
---
+++ ProcessorSim/ProcessorSim/Instructions/CompareI.cs
@@ -0,0 +1,20 @@
+using ProcessorSim.HardwareResources;
+namespace ProcessorSim.Instructions;
+
+public class CompareI
+{
+    private Register flag, reg1;
+    private int value;
+    public CompareI(Register flag, Register register1, int value)
+    {
+        this.flag = flag;
+        this.reg1 = register1;
+        this.value = value;
+    }
+
+    public bool execute()
+    {
+        this.flag.setValue((this.reg1.getValue() == value) ? 1 : 0);
+        return true;
+    }
+}

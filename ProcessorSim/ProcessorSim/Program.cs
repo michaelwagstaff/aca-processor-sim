b1a1eed31e3dfb59d5dc132b18fb4c5b304f92a7
Michael Wagstaff <michaelwagstaff2000@gmail.com>
2022-02-08 14:09:48 +0000

    Added my excessively-complex decode

--- ProcessorSim/ProcessorSim/Program.cs
+++ ProcessorSim/ProcessorSim/Program.cs
@@ -14,8 +14,8 @@ class ProcessorSim
     public static void tick(Resources resources)
     {
         int instructionRegister = fetch(resources);
-        decode(resources, instructionRegister);
-        execute(resources);
+        Instruction decodedInstruction = decode(resources, instructionRegister);
+        execute(resources, decodedInstruction);
     }
 
     public static int fetch(Resources resources)
@@ -37,6 +37,46 @@ class ProcessorSim
     public static Instruction decode(Resources resources, int instructionRegister)
     {
         int rawInstruction = resources.registers[instructionRegister].getValue();
+        rawInstruction = rawInstruction >> 16;
+        // First 6 bits are opcode
+        // Next 5 for operand 1
+        // Final 5 for operand 2
+        int opCode = rawInstruction >> 10;
+        int operand1 = (rawInstruction >> 5) & 31;
+        int operand2 = rawInstruction & 31;
+        if ((opCode >> 5) == 1)
+        {
+            // Arithmetic
+            if ((opCode >> 3 & 3) == 0)
+            {
+                // Add
+            }
+            if ((opCode >> 3 & 3) == 1)
+            {
+                // Subtract
+            }
+            if ((opCode >> 3 & 3) == 2)
+            {
+                // Multiply
+            }
+            if ((opCode >> 3 & 3) == 3)
+            {
+                // Divide
+            }
+        }
+        else
+        {
+            if ((opCode >> 3 & 3) == 0)
+            {
+                // Load
+            }
+
+            if ((opCode >> 3 & 3) == 1)
+            {
+                // Store
+            }
+            // Everything Else
+        }
         // Actually decode
         return new Add();
     }

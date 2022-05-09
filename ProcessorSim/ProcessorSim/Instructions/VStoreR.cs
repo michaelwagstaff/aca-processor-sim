da67db29b2b8e097be3249dc3d8f06d0d3f8c252
Michael Wagstaff <michaelwagstaff2000@gmail.com>
2022-05-09 16:06:50 +0100

    Added VStoreR

--- ProcessorSim/ProcessorSim/Instructions/VStoreR.cs
+++ ProcessorSim/ProcessorSim/Instructions/VStoreR.cs
@@ -2,26 +2,29 @@ using ProcessorSim.Enums;
 using ProcessorSim.HardwareResources;
 namespace ProcessorSim.Instructions;
 
-public class VStore : VInstruction
+public class VStoreR : VInstruction
 {
     public ExecutionTypes executionType { get; set; }
     public VRegister targetRegister { get; set; }
     public int[] result { get; set; }
     public int registerFile { get; set; }
     public List<VRegister> inputRegisters { get; set; }
+    public Register memoryIndexRegister { get; set; }
     public int reorderBuffer { get; set; }
     private VRegister reg;
     public int memoryIndex { get; set; }
-    public VStore(VRegister register, int memoryIndex)
+    public VStoreR(VRegister register, Register memoryIndex)
     {
         inputRegisters = new List<VRegister>();
-        this.inputRegisters.Add(register);
-        this.memoryIndex = memoryIndex;
+        inputRegisters.Add(register);
+        this.reg = register;
+        this.memoryIndexRegister = memoryIndex;
         this.executionType = ExecutionTypes.LoadStore;
     }
     public bool execute(Resources resources, List<int[]> args)
     {
         result = args[1];
+        memoryIndex = args[0][0];
         return true;
     }
 }
